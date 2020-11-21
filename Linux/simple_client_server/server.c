e5d960a667791e84c4b338a3221f7e6e32d95034
moth96 <312041716@qq.com>
2020-11-21 14:19:27 +0800

    update

--- Linux/simple_client_server/server.c
+++ Linux/simple_client_server/server.c
@@ -14,20 +14,40 @@ int main(void)
     struct sockaddr_in serv_addr, client_addr;
     socklen_t client_addr_len, clinet_IP_len;
     char buf[BUFSIZ], client_IP[BUFSIZ];
-    int n,i;
+    int n, i, ret;
     
     lfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (lfd == -1)
+	{
+		perror("socket error");
+		exit(1);
+	}
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(SERV_PORT);
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    bind(lfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+    ret = bind(lfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+	if (ret == -1)
+	{
+		perror("bind error");
+		exit(1);
+	}
 
-    listen(lfd, 128);
+    ret = listen(lfd, 128);
+	if (ret == -1)
+	{
+		perror("listen error");
+		exit(1);
+	}
 
     client_addr_len = sizeof(client_addr);
     cfd = accept(lfd, (struct sockaddr *) &client_addr, &client_addr_len);
+	if (cfd == -1)
+	{
+		perror("accept error");
+		exit(1);
+	}
 
     printf("client IP : %s , client port : %d \n", inet_ntop(AF_INET, 
     	&client_addr.sin_addr.s_addr, client_IP, sizeof(client_IP)), 
